616cb0eaed81940739f0db813193907fe2906620
YaRo <roman.yatsenko@hneu.net>
2023-09-28 19:08:55 +0300

    Add posts to index

--- board/views.py
+++ board/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+from .models import BoardMessage
+
 def index(request):
-    return HttpResponse("Тут буде список оголошень.")
+    content = 'Список оголошень\n\n'
+    for post in BoardMessage.objects.order_by('-published'):
+        content += post.title + '\n' + post.content +'\n\n'
+    return HttpResponse(content, content_type='text/plain; charset=utf-8')
     
