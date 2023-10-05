f9a648d8fc30d08bf3e5160f67d4ef1584827613
YaRo <roman.yatsenko@hneu.net>
2023-10-05 19:54:12 +0300

    Add by_rubric page

--- board/views.py
+++ board/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
-from .models import BoardMessage
+from .models import BoardMessage, Rubric
 
 def index(request):
     posts = BoardMessage.objects.all()
-    context = {'posts': posts}
+    rubrics = Rubric.objects.all()
+    context = {
+        'posts': posts,
+        'rubrics': rubrics,
+    }
     return render(request, 'board/index.html', context)
-            
+
+
+def by_rubric(request, rubric_id):
+    posts = BoardMessage.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)        
+    # context = {
+    #     'post': posts,
+    #     'rubrics': rubrics,
+    #     'current_rubrics': current_rubric,
+    # }
+    # return render(request, 'board/by_rubric.html', context)
+    return render(request, 'board/by_rubric.html', locals())
