c3cc0dc1de90f2866c62f9e8ad67c18b502ed7ae
YaRo <roman.yatsenko@hneu.net>
2023-10-05 19:18:13 +0300

    Add Rubric str method

--- board/models.py
+++ board/models.py
@@ -18,6 +18,9 @@ class BoardMessage(models.Model):
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Назва')
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         verbose_name = 'Рубрика'
         verbose_name_plural = 'Рубрики'
